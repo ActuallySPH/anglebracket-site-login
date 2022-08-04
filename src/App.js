e84113855b9b641dffccf818db3f9fefb6b5f54c
“Suraj <sputhran98@gmail.com>
2022-08-04 12:54:30 +0530

    Fixed failure

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
           <Route
             path='/register'
             element={
-              <FromNew
+              <Form
                 title="Register"
                 setEmail={setEmail}
                 setPassword={setPassword}
